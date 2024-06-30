ab357c598264332cbbb145a4dd64ed842fb4348a
ananthapadmanabhan-o <ananthan51ah@gmail.com>
2024-06-30 17:44:47 +0530

    team assigner

--- tracker/tracker.py
+++ tracker/tracker.py
@@ -180,10 +180,13 @@ class Tracker:
             referee_dict = tracks['referees'][frame_num]
 
             for track_id, player in player_dict.items():
-                frame = self.draw_ellipse(frame,player['bbox'],(0,0,255),track_id)
+                color = player.get('team_color',(0,0,255))
+                frame = self.draw_ellipse(frame,player['bbox'],color,track_id)
 
-            for track_id, referee in referee_dict.items():
-                frame = self.draw_ellipse(frame,referee['bbox'],(0,255,255),track_id)
+
+
+            for _, referee in referee_dict.items():
+                frame = self.draw_ellipse(frame,referee['bbox'],(0,255,255))
 
             for track_id, ball in ball_dict.items():
                 frame = self.draw_traingle(frame,ball['bbox'],(0,255,0))
